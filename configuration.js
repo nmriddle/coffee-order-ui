bdc7b12f8a6eecab27215bd4e92f450f680a2181
nmriddle <nmriddle@iu.edu>
2024-04-14 14:47:10 -0400

    security stuff

---
+++ configuration.js
@@ -0,0 +1,85 @@
+const mode = 1;
+const host_local = "http://localhost:8080";
+const host_remote = "https://coffee-order-latest-lfzd.onrender.com";
+
+function getHost() {
+  return mode == 0 ? host_local : host_remote;
+}
+function isLoggedIn() {
+  if (localStorage.getItem("token")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getTheToken() {
+  return localStorage.getItem("token");
+}
+
+function saveTheToken(token) {
+  localStorage.setItem("token", token);
+  updateTheNavigationBar();
+}
+
+function removeTheToken() {
+  localStorage.removeItem("token");
+  updateTheNavigationBar();
+}
+
+let configuration = {
+  isLoggedIn: () => isLoggedIn(),
+  host: () => getHost(),
+  token: () => getTheToken(),
+};
+
+updateTheNavigationBar();
+
+async function updateTheNavigationBar() {
+  const navigation = document.getElementsByClassName("right-align")[0];
+
+  let loginTag = navigation.children[navigation.children.length - 1];
+  if (configuration.isLoggedIn()) {
+    loginTag.innerHTML = `<a href="login.html">Logout</a>`;
+  } else {
+    loginTag.innerHTML = '<a href="login.html">Login</a>';
+  }
+}
+
+async function login() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  let customer = { username: username, password: password };
+  let request = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(customer),
+  };
+  try {
+    let response = await fetch(getHost() + "/signin", request);
+    if (response.status == 200) {
+      alert("The login was successful!");
+
+      const token = await response.text();
+      saveTheToken(token);
+      location.href = "index.html";
+    } else {
+      location.href = "index.html";
+
+      console.log("response status: ${response.status}");
+      removeTheToken();
+      alert("Something went wrong!");
+    }
+  } catch (error) {
+    console.log(error);
+    removeTheToken();
+    alert("Something went wrong!");
+  }
+}
+
+async function logout() {
+  removeTheToken();
+}
